9013533412dc0f1d0ab0661afcf4a4d1d69cbb6e
JeremyRoalef <roalef944@morrisville.edu>
2026-04-20 12:00:43 -0400

    03) Comments

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Hello, World!");
+
+/*
+Console.WriteLine("Hello, World!");
+*/
+
+
+//Print the value of a number
+Console.WriteLine(10);
