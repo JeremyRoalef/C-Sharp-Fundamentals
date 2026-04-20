f4f3fc68fa02c17d03b4d0147957632070926394
JeremyRoalef <roalef944@morrisville.edu>
2026-04-20 11:44:55 -0400

    02) Data Types & Variables

---
+++ DataTypes&Variables/DataTypes&Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿int myNumber = 10;
+Console.WriteLine(myNumber);
+
+float myFloatingNumber = 2.5f;
+Console.WriteLine(myFloatingNumber);
+
+decimal myDecimalNumber = 10.94312483213m;
+Console.WriteLine(myDecimalNumber);
+
+double myDoubleNumber = 10.42;
+Console.WriteLine(myDoubleNumber);
+
+
+char myChar = 'c';
+Console.WriteLine(myChar);
+
+string myString = "Hello World!";
+Console.WriteLine(myString);
+
+bool myBool = true;
+Console.WriteLine(myBool);
+
+bool myBool2 = false;
+Console.WriteLine(myBool2);
