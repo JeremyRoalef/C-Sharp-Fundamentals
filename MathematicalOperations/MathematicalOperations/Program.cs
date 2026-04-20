1285d3b3f7d8d82f14da0c0b9656b8dc52cb3ae2
JeremyRoalef <roalef944@morrisville.edu>
2026-04-20 12:21:22 -0400

    04) Mathematical Operations

---
+++ MathematicalOperations/MathematicalOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿//Print the sum of two integers
+int sum = 2 + 3;
+Console.WriteLine(sum);
+
+//Print out the sum of two floating-point numbers
+float sumFloat = 4.0f + 9.9f;
+Console.WriteLine(sumFloat);
+
+//Print the difference between two doubles
+Console.WriteLine(5.932 - 1.999);
+
+//Print the multple of 2 and 3
+Console.WriteLine(2 * 3);
+
+//Print the division of 9.9 and 3.3
+Console.WriteLine(9.9f / 3.3f);
+
+//Divide the integer 4 by 3
+Console.WriteLine(7 / 3);
+
+//Divide two integer variables by each other by casting one to a float
+int x = 10;
+int y = 4;
+Console.WriteLine((float)x / y);
+
+//Find the remainder of the integer division
+Console.WriteLine(x % y);
+
+//Find the remainder between two floating point numbers
+float xFloat = 9.99f;
+float yFloat = 4.44f;
+Console.WriteLine(xFloat % yFloat);
