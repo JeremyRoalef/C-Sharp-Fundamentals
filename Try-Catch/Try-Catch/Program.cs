958d55080523f840fff81bd0bb1d33c67fb35041
JeremyRoalef <roalef944@morrisville.edu>
2026-04-24 13:20:17 -0400

    8) Try-Catch Statements

---
+++ Try-Catch/Try-Catch/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    //Trying to parse this string will fail, causeing a System.FormatException error
+    int.Parse("wergfeeqf");
+    int.Parse("woeohgweg");
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
