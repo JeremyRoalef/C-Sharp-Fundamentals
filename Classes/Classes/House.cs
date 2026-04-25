c2ba228c1d6795fbe53e4222f6afec9f68c4cbe0
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 13:02:29 -0400

    16) Class Declaration

---
+++ Classes/Classes/House.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes
+{
+    public class House
+    {
+        //Create some attributes
+        public float length;
+        public float width;
+        public int numOfRooms;
+
+        private string houseAddress;
+
+        public float GetArea()
+        {
+            return length * width;
+        }
+
+        private float GetPerimeter()
+        {
+            return (2 * width) + (2 * length);
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using Classes;
+
+public class Program
+{
+    //This will always run when the application runs
+    static void Main(string[] args)
+    {
+        House myHouse = new House();
+        myHouse.length = 10;
+        myHouse.width = 5;
+        myHouse.numOfRooms = 6;
+
+        Console.WriteLine($"Length: {myHouse.length}\n" +
+            $"Width: {myHouse.width}\n" +
+            $"Number of Rooms: {myHouse.numOfRooms}");
+
+        Console.WriteLine("---------------------------------------");
+
+        House myFirstHouse = myHouse;
+
+        myHouse = new House();
+
+        Console.WriteLine($"Length: {myHouse.length}\n" +
+            $"Width: {myHouse.width}\n" +
+            $"Number of Rooms: {myHouse.numOfRooms}");
+
+        Console.WriteLine("---------------------------------------");
+
+        Console.WriteLine($"Length: {myFirstHouse.length}\n" +
+            $"Width: {myFirstHouse.width}\n" +
+            $"Number of Rooms: {myFirstHouse.numOfRooms}");
+
+        myHouse = null;
+
+        Console.WriteLine("---------------------------------------");
+
+        Console.WriteLine(myFirstHouse.GetArea());
+    }
+}
