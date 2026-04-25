37e3db6e58e7407ba25dddbe2e5fa5c66a632bcb
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 13:26:02 -0400

    18) Overriding ToString()

---
+++ OverridingToString/OverridingToString/Computer.cs
@@ -0,0 +1,27 @@
+﻿namespace OverridingToString
+{
+    public class Computer
+    {
+        string displayRatio;
+        string keyboardLayout;
+        string model;
+
+        //Initialize the computer's attributes
+        public Computer(
+            string displayRatio, 
+            string keyboardLayout, 
+            string model)
+        {
+            this.displayRatio = displayRatio;
+            this.keyboardLayout = keyboardLayout;
+            this.model = model;
+        }
+
+        public override string ToString()
+        {
+            return $"Display Ratio: {displayRatio}\n" +
+                $"Keyboard Layout: {keyboardLayout}\n" +
+                $"Model: {model}";
+        }
+    }
+}
---
+++ OverridingToString/OverridingToString/Program.cs
@@ -0,0 +1,10 @@
+﻿using OverridingToString;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Computer myComputer = new Computer("16:9", "QWERTY", "Lenovo");
+        Console.WriteLine(myComputer.ToString());
+    }
+}
