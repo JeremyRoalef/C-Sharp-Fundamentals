02f8a7466106fa7eb6523af05f9727b4bfa619ca
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 13:49:33 -0400

    19) Overloading Methods

---
+++ Overloading Methods/Overloading Methods/Cup.cs
@@ -0,0 +1,44 @@
+﻿namespace Overloading_Methods
+{
+    public class Cup
+    {
+        float radius;
+        float height;
+        bool hasHandle;
+
+        public Cup(float radius) : this(radius, 0)
+        {
+            //this.radius = radius;
+            //this.height = 0;
+            //hasHandle = false;
+
+            Console.WriteLine("First constructor was called");
+        }
+        public Cup(float radius, float height) : this(radius, height, false)
+        {
+            //this.radius = radius;
+            //this.height = height;
+            //hasHandle = false;
+
+            Console.WriteLine("Second constructor was called");
+        }
+        public Cup(float radius, float height, bool hasHandle)
+        {
+            this.radius = radius;
+            this.height = height;
+            this.hasHandle = hasHandle;
+
+            Console.WriteLine("Third constructor was called");
+        }
+
+        public override string ToString()
+        {
+            string handleMessage = hasHandle ?
+                "Cup has a handle" : "Cup has no handle";
+
+            return $"Radius: {radius}\n" +
+                $"Height: {height}\n" +
+                $"{handleMessage}";
+        }
+    }
+}
---
+++ Overloading Methods/Overloading Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using Overloading_Methods;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Create the cups using a different overloaded constructor
+        Cup cup1 = new Cup(10f);
+
+        Console.WriteLine("----------------------------------");
+
+        Cup cup2 = new Cup(7f, 3f);
+
+        Console.WriteLine("----------------------------------");
+
+        Cup cup3 = new Cup(9f, 4.5f, true);
+
+        Console.WriteLine("----------------------------------");
+
+        //Display the cups to the console
+        Console.WriteLine(cup1.ToString());
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine(cup2.ToString());
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine(cup3.ToString());
+    }
+}
