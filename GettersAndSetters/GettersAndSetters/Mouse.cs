7ed7dfb8beb946ce4365e267eac3680c59f014ec
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 14:07:49 -0400

    20) Getter & Setter Methods

---
+++ GettersAndSetters/GettersAndSetters/Mouse.cs
@@ -0,0 +1,42 @@
+﻿namespace GettersAndSetters
+{
+    public class Mouse
+    {
+        int numOfButtons;
+        int numOfDPISettings;
+        bool isWired;
+        bool isErgonomic;
+
+        //Initialize Mouse attributes
+        public Mouse(
+            int numOfButtons, 
+            int numOfDPISettings, 
+            bool isWired, 
+            bool isErgonomic)
+        {
+            this.numOfButtons = numOfButtons;
+            this.numOfDPISettings = numOfDPISettings;
+            this.isWired = isWired;
+            this.isErgonomic = isErgonomic;
+        }
+
+        //Create the getter methods
+        public int GetNumOfButtons() => numOfButtons;
+        public int GetnumOfDPISettings() => numOfDPISettings;
+        public bool GetIsWired() => isWired;
+        public bool GetIsErgonomic() => isErgonomic;
+
+        //Create the setter methods
+        public void SetNumOfButtons( int numOfButtons) 
+            => this.numOfButtons = numOfButtons;
+
+        public void SetNumOfDPISettings(int numOfDPISettings) 
+            => this.numOfDPISettings = numOfDPISettings;
+
+        public void SetIsWired(bool isWired) 
+            => this.isWired = isWired;
+
+        public void SetIsErgonomic(bool isErgonomic)
+            => this.isErgonomic = isErgonomic;
+    }
+}
---
+++ GettersAndSetters/GettersAndSetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using GettersAndSetters;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Mouse myMouse = new Mouse(10, 3, true, true);
+
+        //Get the attributes from my mouse
+        Console.WriteLine($"Number of mouse buttons: {myMouse.GetNumOfButtons()}");
+        Console.WriteLine($"Number of DPI settings: {myMouse.GetnumOfDPISettings()}");
+        Console.WriteLine($"Is the mouse wired? {myMouse.GetIsWired()}");
+        Console.WriteLine($"Is the mouse ergonomic? {myMouse.GetIsErgonomic()}");
+
+        Console.WriteLine("------------------------------------------------");
+
+        //Set the attributes for my mouse
+        myMouse.SetNumOfButtons(14);
+        myMouse.SetNumOfDPISettings(0);
+        myMouse.SetIsWired(false);
+        myMouse.SetIsErgonomic(false);
+
+        Console.WriteLine("------------------------------------------------");
+
+        //Get the attributes from my mouse
+        Console.WriteLine($"Number of mouse buttons: {myMouse.GetNumOfButtons()}");
+        Console.WriteLine($"Number of DPI settings: {myMouse.GetnumOfDPISettings()}");
+        Console.WriteLine($"Is the mouse wired? {myMouse.GetIsWired()}");
+        Console.WriteLine($"Is the mouse ergonomic? {myMouse.GetIsErgonomic()}");
+    }
+}
