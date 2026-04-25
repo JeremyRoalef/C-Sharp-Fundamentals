fec7b63482bf43ea9c03f5527ee28e9ac565bdaf
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 13:17:43 -0400

    17) OOP - The Constructor

---
+++ TheConstructor/TheConstructor/Animal.cs
@@ -0,0 +1,24 @@
+﻿namespace TheConstructor
+{
+    public class Animal
+    {
+        public int numOfLegs;
+        public string name;
+
+        //Constructor syntax
+        public Animal()
+        {
+            //Initialize the object's attributes
+            this.numOfLegs = 10;
+            this.name = "Unknown";
+
+            //Initialize the object's behavior
+            DisplayMessage();
+        }
+
+        void DisplayMessage()
+        {
+            Console.WriteLine("Animal has been created!");
+        }
+    }
+}
---
+++ TheConstructor/TheConstructor/Program.cs
@@ -0,0 +1,14 @@
+﻿using TheConstructor;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Create the animal pointer & object 
+        Animal myAnimal = new Animal();
+
+        //Display animal properties from the constructor
+        Console.WriteLine($"Number of legs: {myAnimal.numOfLegs}\n" +
+            $"Animal Name: {myAnimal.name}");
+    }
+}
