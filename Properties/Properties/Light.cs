c8f6cba9c5a16dbe219b4e4828718eff956247c4
JeremyRoalef <roalef944@morrisville.edu>
2026-04-25 16:21:30 -0400

    21) Properties

---
+++ Properties/Properties/Light.cs
@@ -0,0 +1,34 @@
+﻿namespace Properties
+{
+    public class Light
+    {
+        string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public float Intensity
+        {
+            get;
+            private set;
+        }
+
+        int position;
+        public int Position
+        {
+            set
+            {
+                if (value < 0) return;
+
+                position = value;
+                Console.WriteLine("The value of the light's position has changed");
+            }
+            get 
+            {  
+                return position; 
+            }
+        }
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,19 @@
+﻿using Properties;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Light myLight = new Light();
+
+        //Set the light's color, then display it
+        myLight.Color = "Red";
+        Console.WriteLine($"Light color: {myLight.Color}");
+
+        //Get the intensity of the light
+        Console.WriteLine($"Light intensity: {myLight.Intensity}");
+
+        //Set the position of the light
+        myLight.Position = -10;
+    }
+}
