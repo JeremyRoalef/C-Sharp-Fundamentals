e565bd7aba0a7d45a66042ef09924ad5df8a0521
JeremyRoalef <roalef944@morrisville.edu>
2026-04-29 14:06:37 -0400

    26) Enums

---
+++ DataTypes/Enums/Enums/Enemy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public class Enemy
+    {
+        //Define the enemy intent enum
+        public enum Intent
+        {
+            Attack, //array index 0
+            Heal, //array index 1
+            ApplyDebuffToPlayer, //array index 2
+            ApplyBuffToSelf //array index 3
+        }
+
+        public Intent[] EnemyIntent { get; private set; }
+
+        public Enemy(Intent[] defaultIntent)
+        {
+            EnemyIntent = defaultIntent;
+        }
+
+        public void DoAction()
+        {
+            //Loop through all of the enemy's current intentions and perform the intention
+            foreach (Intent currentIntent in EnemyIntent)
+            {
+                //Switch-case on an enum's value
+                switch (currentIntent)
+                {
+                    case Intent.Attack:
+                        Console.WriteLine("The enemy attacks the player");
+                        break;
+                    case Intent.Heal:
+                        Console.WriteLine("The enemy heals");
+                        break;
+                    case Intent.ApplyDebuffToPlayer:
+                        Console.WriteLine("The enemy applies a debuff to the player");
+                        break;
+                    case Intent.ApplyBuffToSelf:
+                        Console.WriteLine("The enemy applies a buff to itself");
+                        break;
+                }
+            }
+
+            StartNewIntent();
+        }
+
+        void StartNewIntent()
+        {
+            //Randomize the number of intentions for the enemy's next action
+            int numOfIntents = Random.Shared.Next(1, 3);
+
+            //Create an array of all enemy intentions for the next action
+            Intent[] newIntent = new Intent[numOfIntents];
+
+            //Randomly select an intention, and store it in the enemy's intention array
+            for (int i = 0; i < numOfIntents; i++)
+            {
+                int randomNum = Random.Shared.Next(0, 4);
+                Intent currentIntent = Enum.GetValues<Intent>()[randomNum];
+
+                //Add a new intent to the array of enemy intentions
+                newIntent[i] = currentIntent;
+            }
+            
+            //Set the enemy's intention array to the new array of intentions
+            EnemyIntent = newIntent;
+        }
+    }
+}
---
+++ DataTypes/Enums/Enums/Program.cs
@@ -0,0 +1,72 @@
+﻿using Enums;
+using static Enums.Enemy;
+
+public class Program
+{
+    //Define the CharacterState enum
+    public enum CharacterState
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G
+    }
+
+    static void Main(string[] args)
+    {
+        //Craete a new variable of the character state enum
+        CharacterState characterState = CharacterState.A;
+        Console.WriteLine($"My current character state is {characterState.ToString()}");
+
+        //Change the value of the stored enum character
+        characterState = CharacterState.E;
+        Console.WriteLine($"My new character state is {characterState.ToString()}");
+
+        Console.WriteLine("-----------------------------------------------------------");
+
+        switch (characterState)
+        {
+            case CharacterState.A:
+                Console.WriteLine("A was selected as the current character state");
+                break;
+            case CharacterState.B:
+                Console.WriteLine("B was selected as the current character state");
+                break;
+            case CharacterState.C:
+                Console.WriteLine("C was selected as the current character state");
+                break;
+            case CharacterState.D:
+                Console.WriteLine("D was selected as the current character state");
+                break;
+            case CharacterState.E:
+                Console.WriteLine("E was selected as the current character state");
+                break;
+        }
+
+        Console.WriteLine("-----------------------------------------------------------");
+        //Randomize the starting intentions of the enemy (see enemy class)
+        int numOfIntents = Random.Shared.Next(1, 3);
+        Intent[] newIntent = new Intent[numOfIntents];
+        for (int i = 0; i < numOfIntents; i++)
+        {
+            int randomNum = Random.Shared.Next(0, 4);
+            Intent currentIntent = Enum.GetValues<Intent>()[randomNum];
+
+            //Add a new intent to the array of enemy intentions
+            newIntent[i] = currentIntent;
+        }
+
+        //Create the new enemy, & have it do some actions
+        Enemy enemy = new Enemy(newIntent);
+        enemy.DoAction();
+        Console.WriteLine("-----------------------------------------------------------");
+        enemy.DoAction();
+        Console.WriteLine("-----------------------------------------------------------");
+        enemy.DoAction();
+        Console.WriteLine("-----------------------------------------------------------");
+        enemy.DoAction();
+    }
+}
