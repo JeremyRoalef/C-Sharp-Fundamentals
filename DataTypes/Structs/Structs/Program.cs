158d2e5b4dcf5b2f2b38968afdf407b75935e951
JeremyRoalef <roalef944@morrisville.edu>
2026-05-01 14:07:20 -0400

    28) Structs

---
+++ DataTypes/Structs/Structs/Program.cs
@@ -0,0 +1,58 @@
+﻿using Structs;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Create a new Vector3
+        Vector3 myFirstVector = new Vector3(10, 10, 10);
+        Console.WriteLine("My vector value is: " + myFirstVector.ToString());
+
+        myFirstVector = UpdateMyVector(myFirstVector);
+        Console.WriteLine("My vector value is: " + myFirstVector.ToString());
+
+        Console.WriteLine("------------------------------------------------------------------");
+        Vector3 vectorToCompare = new Vector3(10, 10, 10);
+        Console.WriteLine("My vector value is: " + vectorToCompare.ToString());
+
+        Console.WriteLine("------------------------------------------------------------------");
+        if (vectorToCompare.Equals(myFirstVector))
+        {
+            Console.WriteLine("My new vector equals the original");
+        }
+        else
+        {
+            Console.WriteLine("My new vector is not equal to the original");
+        }
+
+        Console.WriteLine("------------------------------------------------------------------");
+        Vector3 a = new Vector3(12, 15, 7);
+        Vector3 b = new Vector3(11.5f, 17, 4);
+
+        Console.WriteLine($"Vector a + Vector b = {a + b}");
+        Console.WriteLine($"Vector a - Vector b = {a - b}");
+        Console.WriteLine($"Vector b - Vector a = {b - a}");
+
+        Console.WriteLine("------------------------------------------------------------------");
+        Vector3 a2 = new Vector3(12, 3, 4);
+        Vector3 b2 = new Vector3(6, 1, 2);
+
+        Console.WriteLine($"The distance between a2 and b2 is {Vector3.Distance(a2, b2)}");
+
+        Console.WriteLine("------------------------------------------------------------------");
+        Console.WriteLine($"The zero vector is {Vector3.Zero}");
+        Console.WriteLine($"The one vector is {Vector3.One}");
+        Console.WriteLine($"The right vector is {Vector3.Right}");
+        Console.WriteLine($"The up vector is {Vector3.Up}");
+        Console.WriteLine($"The forward vector is {Vector3.Forward}");
+
+        Console.WriteLine($"The left vector is {-Vector3.Right}");
+        Console.WriteLine($"The down vector is {-Vector3.Up}");
+        Console.WriteLine($"The back vector is {-Vector3.Forward}");
+    }
+
+    public static Vector3 UpdateMyVector(Vector3 myVector)
+    {
+        return new Vector3(12, myVector.y, myVector.z);
+    }
+}
---
+++ DataTypes/Structs/Structs/Vector3.cs
@@ -0,0 +1,81 @@
+﻿namespace Structs
+{
+    public struct Vector3 : IEquatable<Vector3>
+    {
+        public float x, y, z;
+
+        public static Vector3 Zero = new Vector3(0, 0, 0);
+        public static Vector3 One = new Vector3(1, 1, 1);
+        public static Vector3 Right = new Vector3(1, 0, 0);
+        public static Vector3 Up = new Vector3(0, 1, 0);
+        public static Vector3 Forward = new Vector3(0, 0, 1);
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x; 
+            this.y = y; 
+            this.z = z;
+        }
+
+        public bool Equals(Vector3 other)
+        {
+            return other.x == x && 
+                other.y == y &&
+                other.z == z;
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(
+                    a.x + b.x,
+                    a.y + b.y,
+                    a.z + b.z
+                );
+        }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(
+                    a.x - b.x,
+                    a.y - b.y,
+                    a.z - b.z
+                );
+        }
+
+        public static Vector3 operator -(Vector3 a)
+        {
+            return new Vector3(
+                -a.x,
+                -a.y,
+                -a.z
+                );
+        }
+
+        public static Vector3 operator * (float constant, Vector3 vector)
+        {
+            return new Vector3(
+                constant * vector.x,
+                constant * vector.y,
+                constant * vector.z
+                );
+        }
+
+        public override string ToString()
+        {
+            // (x, y, z)
+            return $"({x}, {y}, {z})";
+        }
+
+        public static float Distance(Vector3 a, Vector3 b)
+        {
+            Vector3 deltaVector = b - a;
+            float magnitude = MathF.Sqrt(
+                (deltaVector.x * deltaVector.x) +
+                (deltaVector.y * deltaVector.y) +
+                (deltaVector.z * deltaVector.z)
+                );
+
+            return magnitude;
+        }
+    }
+}
